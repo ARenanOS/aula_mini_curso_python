d02ea888593e79886f65a944130d467fd5b7235a
Renan_Oliveira <renan.oliveira@pol.adv.br>
2024-11-06 23:01:46 -0300

    Primeiro Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import DAO
+
+# Chamada de metodos - Executar um comando por VEZ
+
+# Teste de inserção
+#DAO.inserir_usuario('Amanda', 'amanda@teste.com', )
+#DAO.inserir_usuario('Natan', 'natan@teste.com', )
+#DAO.inserir_usuario('José', 'jose@teste.com', )
+#DAO.inserir_usuario('Renan', 'renan@teste.com', )
+
+# Teste de consulta
+# saida = DAO.buscar_usuario('Amanda')
+# print(saida)
+
+# Teste de listar todos
+#saida = DAO.listar_usuarios()
+# print(saida)
+
+
+saida = DAO.buscar_usuario_id(3)
+print(saida)
+
+
+# Teste de atualização
+# saida = DAO.atualizar_usuario(1,'amanda_Testando','amanda_testando@teste.com')
+# print(saida)
+#
+# Teste de remoção
+# saida = DAO.deletar_usuario(1)
+# print(saida)
